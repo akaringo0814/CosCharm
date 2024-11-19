540b8d5b2d90ddae909d583ca4466d6a5e6c5445
“akaringo0814” <akari08141997@gmail.com>
2024-11-19 22:21:59 +0900

    新規アカウント登録

--- CosCharm/app/views.py
+++ CosCharm/app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect 
 from django.views import View
+from app.forms import Signupform 
+from django.contrib.auth import login
 
 # Create your views here.
 
@@ -9,7 +11,19 @@ class PortfolioView(View):
     
 class SignupView(View): 
     def get(self,request):
-        return render(request, "signup.html")
+        form = Signupform()
+        return render(request, "signup.html", context={
+            "form":form
+        })
+    def post(self, request):
+        form = Signupform(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+        return render(request, "signup.html", context={
+            "form": form 
+        })
     
 class LoginView(View): 
     def get(self,request):
